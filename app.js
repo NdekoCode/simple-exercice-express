75ebfa8be65495348e5f7700bdfc36e2c0f9b83f
NdekoCode <arickbulakali@gmail.com>
2022-10-19 15:59:43 +0200

    fixed: add main dependances and main file

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+function loadViewFile(viewFile) {
+  path.join(__dirname, "views", viewFile + ".html");
+}
+app.get("/", (req, res) => {
+  res.sendFile(path.join(loadViewFile("home")));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(loadViewFile("about"));
+});
+app.get("/contact", (req, res) => {
+  res.sendFile(loadViewFile("contact"));
+});
+
+app.listen(process.env.PORT || 8080);
